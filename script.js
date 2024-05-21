1a3c28cba1ca9dd922f288028c2c7cc68a725aa5
neeraj-kumar-94 <neeraj74530@gmail.com>
2024-05-21 19:27:34 +0530

    add footer section

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".testimonial-slide-inner-sec", {
+    cssMode: true,
+    freeMode: true,
+    slidesPerView: 2.8,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
